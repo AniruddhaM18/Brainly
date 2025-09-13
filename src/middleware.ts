f69cc79feb58be5858a4e351edc22f6056304e59
Aniruddha <aniruddhamaradwar9@gmail.com>
2025-09-13 23:27:30 +0530

    added middlewares

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,14 +6,3 @@ import { JWT_PASSWORD } from "./config.js";
 export const UserMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const header = req.headers["authorization"];
     const decoded = Jwt.verify(header as string, JWT_PASSWORD)
-
-    if(decoded) {
-        //@ts-ignore
-        req.userId = decoded.id;
-        next()
-    } else {
-        res.status(403).json({
-            message: "You aren't LOGGED IN BR0"
-        })
-    }
-}
